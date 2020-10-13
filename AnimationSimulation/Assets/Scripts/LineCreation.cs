4e403371ca74ae54e4691f67484805ca25dfae0a
Az <thomas.guyamier+Kraken@gmail.com>
2020-10-13 09:44:49 +0200

    Création d'une ligne qui suis une target

---
+++ AnimationSimulation/Assets/Scripts/LineCreation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreation : MonoBehaviour
+{
+    public LineRenderer line;
+    public Transform targetPoints;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePoint(line, targetPoints);
+    }
+
+    public static void MovePoint(LineRenderer line, Transform target) 
+    {
+        // Move Last Index
+        line.SetPosition(line.positionCount -1, target.position);
+    }
+}
